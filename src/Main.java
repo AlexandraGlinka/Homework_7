7fea37acf02a8788a3eea2d02e0693d5d2dd5796
glinka <alexandraglinka@yandex.ru>
2023-03-02 22:23:03 +0300

    Task_2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
+        task2();
     }
 
     public static void task1 () {
@@ -23,5 +23,26 @@ public class Main {
         //System.out.println("Месяц " + month + ", сумма накоплений равна "+ total + " рублей");
     }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+        /*Выведите в одну строку через пробел числа от 1 до 10 с помощью цикла while.
+        На следующей строке выведите числа в обратном порядке от 10 до 1 с помощью цикла for.
+        Для обоих циклов можно использовать как одну общую переменную, так и в каждом цикле свою.
+        Не забудьте выполнить переход на новую строку между двумя циклами.
+        В результате программы вывод должен получиться следующий:*/
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        System.out.println();
+
+        for (int j = 10; j > 0; j -= 1) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
 
 }
