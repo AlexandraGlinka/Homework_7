200d15be09efb4be4d5c1c5bf2a66476ab553de3
glinka <alexandraglinka@yandex.ru>
2023-03-02 22:16:44 +0300

    Task_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        //task2();
     }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        /*С помощью цикла while посчитайте, сколько месяцев потребуется,
+        чтобы накопить 2 459 000 рублей при условии, что первоначально мы имеем 0 рублей
+        и готовы откладывать по 15 тысяч рублей.*/
+
+        int total = 0;
+        int accum = 15_000;
+        int month = 0;
+
+        while (total < 2_459_000) {
+            total += accum;
+            month ++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна "+ total + " рублей");
+        }
+
+        //System.out.println("Месяц " + month + ", сумма накоплений равна "+ total + " рублей");
+    }
+
+
 }
